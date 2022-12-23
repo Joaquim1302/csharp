cc3db058ffdb2e5f376c8e898ff9d8ebe3c487d6
Joaquim Marques <91548193+Joaquim1302@users.noreply.github.com>
2022-12-23 15:43:07 -0300

    139 c# carregar imagem por codigo em c#

---
+++ 138 c# introducao a utilizacao de imagem em c#/ProjetoImagem/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoImagem
+{
+    public partial class Form1 : Form
+    {
+        string pasta_images = "";
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+            pasta_images = Application.StartupPath + @"\images\";
+            // carregamento da imagem de fundo
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackgroundImage = Image.FromFile(pasta_images + "foto_1.jpg");
+        }
+    }
+}
