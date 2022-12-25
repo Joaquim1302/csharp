27e186f11123ec2f2b7bc8cac63c0bfd45250968
Joaquim Marques <91548193+Joaquim1302@users.noreply.github.com>
2022-12-25 11:34:07 -0300

    143 c# principios basicos para criacao de imagens

---
+++ 143 c# principios basicos para criacao de imagens/ProjetoImagem/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ProjetoImagem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_teste = new System.Windows.Forms.PictureBox();
+            this.cmd_teste = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_teste)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmd_teste)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_teste
+            // 
+            this.pic_teste.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_teste.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic_teste.Location = new System.Drawing.Point(12, 12);
+            this.pic_teste.Name = "pic_teste";
+            this.pic_teste.Size = new System.Drawing.Size(689, 320);
+            this.pic_teste.TabIndex = 0;
+            this.pic_teste.TabStop = false;
+            this.pic_teste.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pic_teste.DoubleClick += new System.EventHandler(this.pic_teste_DoubleClick);
+            // 
+            // cmd_teste
+            // 
+            this.cmd_teste.BackColor = System.Drawing.Color.Transparent;
+            this.cmd_teste.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cmd_teste.Location = new System.Drawing.Point(551, 350);
+            this.cmd_teste.Name = "cmd_teste";
+            this.cmd_teste.Size = new System.Drawing.Size(150, 40);
+            this.cmd_teste.TabIndex = 0;
+            this.cmd_teste.TabStop = false;
+            this.cmd_teste.Click += new System.EventHandler(this.pic_teste_DoubleClick);
+            this.cmd_teste.MouseEnter += new System.EventHandler(this.cmd_teste_MouseEnter);
+            this.cmd_teste.MouseLeave += new System.EventHandler(this.cmd_teste_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(720, 399);
+            this.Controls.Add(this.cmd_teste);
+            this.Controls.Add(this.pic_teste);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_teste)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmd_teste)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_teste;
+        private System.Windows.Forms.PictureBox cmd_teste;
+    }
+}
+
---
+++ 143 c# principios basicos para criacao de imagens/ProjetoImagem/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoImagem
+{
+    public partial class Form1 : Form
+    {
+        string pasta_images = "";
+        Image img_fundo;
+
+        Image img_normal;   // botão normal
+        Image img_high;     // quando o mouse passa por cima do botão
+        Image img_disabled; // quando o botão está inativo
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+            pasta_images = Application.StartupPath + @"\images\";
+            // carregamento da imagem de fundo
+            img_fundo = Image.FromFile(pasta_images + "foto_1.jpg");
+
+            // carregamento da imagem do botão
+            //pic_teste.Image = img_fundo;
+
+            img_normal = Image.FromFile(pasta_images + "btn_normal.png");
+            img_high = Image.FromFile(pasta_images + "btn_high.png");
+            img_disabled = Image.FromFile(pasta_images + "btn_disabled.png");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //int imgW = img_fundo.Width;
+            //int imgH = img_fundo.Height;
+
+            //this.Size = new Size(671, 449);
+            //this.BackgroundImage = img_fundo;
+
+
+            //Rectangle rect = pic_teste.Bounds;
+
+            cmd_teste.BackgroundImage = img_normal;
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //int x = pic_teste.Location.X;
+            //int y = pic_teste.Location.Y;
+            //pic_teste.Location = new Point(x + 10, y - 10);
+
+        }
+
+        private void pic_teste_DoubleClick(object sender, EventArgs e)
+        {
+            // pic_teste.Location = new Point(pic_teste.Location.X - 10, pic_teste.Location.Y + 10);
+
+            // criar a folha em branco
+            Bitmap imgback = new Bitmap(pic_teste.Width, pic_teste.Height);
+            Graphics desenhador =  Graphics.FromImage(imgback);
+
+            // atribuir uma cor de fundo a imagem
+            desenhador.Clear(Color.Yellow);
+
+            // apresenta imagem final
+            pic_teste.BackgroundImage = imgback;
+
+        }
+
+        private void cmd_teste_MouseEnter(object sender, EventArgs e)
+        {
+            cmd_teste.BackgroundImage = img_high;
+        }
+
+        private void cmd_teste_MouseLeave(object sender, EventArgs e)
+        {
+            cmd_teste.BackgroundImage = img_normal;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ativa e desativa o botão cmd_teste
+            cmd_teste.Enabled = !cmd_teste.Enabled;
+            if (cmd_teste.Enabled) 
+                 cmd_teste.BackgroundImage = img_normal;
+            else
+                cmd_teste.BackgroundImage = img_disabled;
+        }
+    }
+}
